195375e8030d51f216eb7612e57a411552c014b4
d769246859@163.com <d769246859@163.com>
2019-03-16 10:35:56 +0800

    测试数据

---
+++ module-one/src/main/java/com/dangqp/moduleone/method/ArgMethodTest.java
@@ -0,0 +1,13 @@
+package com.dangqp.moduleone.method;
+
+/**
+ * Title:com.dangqp.moduleone.method
+ * Description:
+ * Copyright: Copyright (c) 2019
+ *
+ * @author dangqp
+ * @version 1.0
+ * @created 2019/03/16  10:35
+ */
+public class ArgMethodTest {
+}
